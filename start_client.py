88720681b681a1824ddea93d20cdc8a43a65c988
LOL1322 <112816993+LOL1322@users.noreply.github.com>
2024-03-14 17:07:34 +0300

    Delete start_client.py

--- start_client.py
+++
@@ -1,9 +0,0 @@
-from sys import argv
-from src.client.main_window import MainWindow
-from PySide6.QtWidgets import QApplication
-
-
-if __name__ == "__main__":
-    app = QApplication(argv)
-    root = MainWindow()
-    app.exec()
